70efbbaeb3748220fdc33b8212d771bf8f5a9a96
sanjeev.bhandari <sanjeev.bhandari@tai.com.np>
2026-01-22 11:56:40 +0545

    Upgraded tailwind packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite'
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: 'abc/',
+  base: '/abc',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development'
+    tailwindcss()
   ].filter(Boolean),
   resolve: {
     alias: {
